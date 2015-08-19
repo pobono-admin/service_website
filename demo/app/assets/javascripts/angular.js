4032954f3225eb33d2505593855c2bd5a40bfd7e
pobono-admin <pobono.liren@gmail.com>
2015-08-19 21:09:29 +0800

    test angularjs

---
+++ demo/app/assets/javascripts/angular.js
@@ -0,0 +1,6 @@
+// create angular app
+	var validationApp = angular.module('validationApp', []);
+
+	// create angular controller
+	validationApp.controller('mainController', function($scope) {
+	});
