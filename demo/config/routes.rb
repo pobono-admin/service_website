32671d65add5b3e8e9429046d38893f4094a6966
pobono-admin <pobono.liren@gmail.com>
2015-08-21 23:30:25 +0800

    can not sign up

--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
+  # get 'sessions/new'
+  # get 'users/new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
