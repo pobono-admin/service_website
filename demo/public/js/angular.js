805d9c6ed12d9eedb0123ad43da53416833a4e3e
pobono-admin <pobono.liren@gmail.com>
2015-08-26 17:41:37 +0800

    angular

--- demo/public/js/angular.js
+++ demo/public/js/angular.js
@@ -11,3 +11,20 @@
 	// function mainController($scope) {
 	// 	$scope.name = "Anna";
 	// }
+
+
+
+// 	angular.module('app', [])
+// .controller('PasswordController', function PasswordController($scope) {
+//   $scope.password = '';
+//   $scope.grade = function() {
+//     var size = $scope.password.length;
+//     if (size > 8) {
+//       $scope.strength = 'strong';
+//     } else if (size > 3) {
+//       $scope.strength = 'medium';
+//     } else {
+//       $scope.strength = 'weak';
+//     }
+//   };
+// });
