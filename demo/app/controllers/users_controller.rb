4699f988d7c674813650375000ff534f3595cd54
pobono-admin <pobono.liren@gmail.com>
2015-08-31 16:21:53 +0800

    user edit

--- demo/app/controllers/users_controller.rb
+++ demo/app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController
 
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
+
+
+
+  def logged_in_user
+      unless logged_in?
+        redirect_to login_url
+      end
+  end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+    end
+
+
+
   def new
   	@user = User.new
   end
